ed4884538213579339ebb763fb65bc9c52d689c4
Harlan Iverson <harlan@pubnub.com>
2015-08-21 15:02:28 -0700

    pubnub settings

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-antlr "0.3.0"
+(defproject lein-antlr "0.3.1-pubnub-SNAPSHOT"
   :description "Generate source code from ANTLR grammars in Leiningen."
   :dependencies [[org.antlr/antlr4 "4.5"]]
   :profile {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]}}
@@ -6,4 +6,6 @@
   :eval-in-leiningen true
   :license {:name "Apache Software License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"
-			:distribution :repo})
+            :distribution :repo}
+  :deploy-repositories [["snapshots" "http://maven.devbuild.pubnub.com:8081/artifactory/ext-snapshot-local"]
+                        ["releases" "http://maven.devbuild.pubnub.com:8081/artifactory/ext-release-local"]])
